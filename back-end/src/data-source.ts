c6f836f95a71dc450a4258335e89bf5dfbfe13e6
Sruthy Ramesh <s3988110@student.rmit.edu.au>
2025-05-04 19:46:42 +1000

    its storing data in mysql

--- back-end/src/data-source.ts
+++ back-end/src/data-source.ts
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   port: 3306,
   username: "S3988110",
   password: "Dream@123",
-  database: "matt",
+  database: "S3988110",
   // synchronize: true will automatically create database tables based on entity definitions
   // and update them when entity definitions change. This is useful during development
   // but should be disabled in production to prevent accidental data loss.
