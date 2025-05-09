16454bb6ee31a203292fdea5001d4b406506d207
Srishti Khosla <s3959795@student.rmit.edu.au>
2025-05-09 15:00:12 +1000

    Created users table
    - Added data-source.ts in .gitignore
    
    -used bcryptjs for password hashing before insert
    (npm install bcryptjs)

---
+++ back-end/src/entity/Users.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, BeforeInsert } from "typeorm";
+import * as bcrypt from "bcryptjs";
+
+@Entity()
+export class Users {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string
+
+  @Column()
+  lastName: string
+
+  @Column({unique: true})
+  email: string
+
+  @Column()
+  password: string
+
+  @CreateDateColumn({type: "timestamp"})
+  dateOfJoining: Date
+
+  @BeforeInsert()
+  async hashPassword(){
+    this.password = await bcrypt.hash(this.password,10);
+  }
+}
