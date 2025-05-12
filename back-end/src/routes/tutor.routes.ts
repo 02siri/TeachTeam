00b65a0af1a6055559044398daba6eea491e9b2d
Sruthy Ramesh <s3988110@student.rmit.edu.au>
2025-05-12 14:45:14 +1000

    tutor-dashboard is saving now

--- back-end/src/routes/tutor.routes.ts
+++ back-end/src/routes/tutor.routes.ts
@@ -4,8 +4,10 @@ import { TutorController } from "../controller/TutorController";
 const router = Router();
 const tutorController = new TutorController();
 
+router.get("/apply", (req: Request, res: Response) => tutorController.getByEmail(req, res));
+
+router.post("/apply", (req: Request, res: Response) => tutorController.create(req, res));
+
 router.get("/tutors", (req: Request, res: Response) => tutorController.all(req, res));
-router.get("/tutors", (req: Request, res: Response) => tutorController.getByEmail(req, res));
-router.post("/tutors", (req: Request, res: Response) => tutorController.create(req, res));
 
 export default router;
