5a220ea275f45339e02f1c7105d33d88f1b03e65
SrishtiKhosla <s3959795@student.rmit.edu.au>
2025-05-20 01:00:26 +1000

    Fixed UI for personal details of profile section
    - To be fixed

--- front-end/src/pages/profile.tsx
+++ front-end/src/pages/profile.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import{ Box, Text, useToast} from "@chakra-ui/react";
+import{ Box, Button, Flex, Grid, GridItem, Text, useToast} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { userApi, tutorApi } from "@/services/api";
 import { useAuth } from "@/context/AuthLogic";
@@ -7,6 +7,7 @@ import { AxiosError } from "axios";
 import Header from "@/components/Header";
 import { motion } from "framer-motion";
 import Footer from "@/components/Footer";
+import { label } from "framer-motion/client";
 
 interface TutorData{
     sessionType: string[];
@@ -32,11 +33,38 @@ interface UserProfile{
 
 const MotionBox = motion(Box); 
 
+//defining menu items for profile
+const profileMenuItems = ["Personal Details", "Application Details", "Qualifications & Skills"] as const;
+type ProfileMenuKey = typeof profileMenuItems[number];
+
+//component for profile menu
+interface ProfileHeaderProps{
+activeMenu : ProfileMenuKey;
+onMenuChange: (menu: ProfileMenuKey) => void;
+tutorData: TutorData | null;
+}
+
+const ProfileHeader : React.FC<ProfileHeaderProps> = ({activeMenu, onMenuChange, tutorData}) => (
+    <Flex p={4} boxShadow = "sm" justify ="space-around" mb={4}>
+        {profileMenuItems.map((item)=>(
+            <Button 
+            key={item} 
+            variant = {activeMenu === item ? "solid" : "ghost"}
+            colorScheme="blue"
+            onClick={()=> onMenuChange(item)}
+            isDisabled = {item === "Application Details" && !tutorData}>
+            {item}
+            </Button>
+        ))}
+    </Flex>
+);
+
 const Profile = () => {
     const [profileData, setProfileData] = useState<UserProfile | null>(null);
     const [tutorData, setTutorData] = useState<TutorData | null>(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string|null>(null);
+    const [activeMenu, setActiveMenu] = useState<ProfileMenuKey>("Personal Details");
     const router = useRouter();
     const toast = useToast();
     const {currentUserEmail} = useAuth();
@@ -93,6 +121,180 @@ const Profile = () => {
         return <Box>No Profile Data available</Box>
     }
 
+const renderActiveProfileSection = () => {
+
+    const sectionStyle = {
+        borderWidth: "1px",
+        borderRadius: "md",
+        p: 6
+    };
+
+    const labelStyle = {
+        fontWeight : "bold",
+        fontSize: "xl",
+        mb: 1,
+    };
+
+    switch(activeMenu){
+        case "Personal Details":
+            return (
+            <Box {...sectionStyle}>
+                <Grid templateColumns = "repeat(2, 1fr)" gap ={4}>
+                    <GridItem>
+                        <Text {...labelStyle}>First Name: </Text>
+                         <Box 
+                            width = "50%"
+                            border="1px" 
+                            borderRadius="md" 
+                            p={2}
+                            background="blue.500"
+                            textColor="white"
+                            fontWeight="bold"
+                        >
+                            <Text fontSize="lg">{profileData.firstName}</Text>
+                        </Box>
+                    </GridItem>
+
+                    <GridItem>
+
+                        <Text {...labelStyle}>Last Name: </Text>
+                         <Box 
+                           width = "50%"
+                            border="1px" 
+                            borderRadius="md" 
+                            p={2}
+                            background="blue.500"
+                            textColor="white"
+                            fontWeight="bold"
+                        >
+                             <Text fontSize="lg">{profileData.lastName}</Text>
+                        </Box>
+                     
+                    </GridItem>
+
+                    <GridItem>
+                        <Text {...labelStyle}>Email: </Text>
+                        <Box 
+                           width = "50%"
+                            border="1px" 
+                            borderRadius="md" 
+                            p={2}
+                            background="blue.500"
+                            textColor="white"
+                            fontWeight="bold"
+                        >
+                             <Text fontSize="lg">{profileData.email}</Text>
+                        </Box>
+                        
+                    </GridItem>
+
+                    <GridItem>
+                        <Text {...labelStyle}>Username: </Text>
+                        <Box 
+                           width = "50%"
+                            border="1px" 
+                            borderRadius="md" 
+                            p={2}
+                            background="blue.500"
+                            textColor="white"
+                            fontWeight="bold"
+                        >
+                             <Text fontSize="lg">{profileData.username}</Text>
+                        </Box>
+                    </GridItem>
+
+                    <GridItem>
+                        <Text {...labelStyle }>Date of Joining </Text>
+                            <Box 
+                           width = "50%"
+                            border="1px" 
+                            borderRadius="md" 
+                            p={2}
+                            background="blue.500"
+                            textColor="white"
+                            fontWeight="bold"
+                            >
+                             <Text fontSize="lg">{new Date(profileData.dateOfJoining).toLocaleDateString()}</Text>
+                        </Box>
+                        
+                    </GridItem>
+                </Grid>
+            </Box>
+        );
+        case "Application Details":
+            return tutorData? (
+            <Box {...sectionStyle}>
+
+                 <Grid templateColumns = "repeat(2, 1fr)" gap ={4}>
+
+                    <GridItem>
+                    <Text fontWeight="bold">Role Applied:</Text>
+                    <Text>{tutorData.sessionType}</Text>
+                    </GridItem>
+
+                    <GridItem>
+                    <Text fontWeight="bold">Availability:</Text>
+                    <Text>{tutorData.availability}</Text>
+                    </GridItem>
+
+                    <GridItem colSpan={1}>
+                    <Text fontWeight="bold">Courses Applied:</Text>
+                    <Box pl={2}>
+                        {tutorData.courses.map((course, index)=>(
+                            <Text key ={index}> -{course.courseCode} {course.courseName} </Text>
+                        ))}
+                    </Box>
+                    </GridItem>
+
+                    <GridItem colSpan={1}>
+                    <Text fontWeight="bold">Previous Roles:</Text>
+                    <Box pl={2}>
+                        {tutorData.previousRoles.map((role, index)=>(
+                            <Text key ={index}> -{role}</Text>
+                        ))}
+                    </Box>
+                    </GridItem>
+                </Grid>
+        </Box>
+        ):(
+            <Box p={4}>
+                No Tutor Application Details found.
+            </Box>
+        );
+        case "Qualifications & Skills":
+            return tutorData ? (
+
+            <Box {...sectionStyle}>
+                 <Grid templateColumns = "repeat(2, 1fr)" gap ={4}>
+                    <GridItem colSpan={2}>
+                        <Text fontWeight="bold">Academic Credentials:</Text>
+                        <Box pl={4}>
+                            {tutorData.user?.credentials.length ? (
+                                tutorData.user?.credentials.map((cred, index) => (
+                                    <Text key = {index}>- {cred.qualification} from {cred.institution} ({cred.year}) </Text>
+                                ))
+                            ):
+                            (
+                                <Text>No Credentials Submitted</Text>
+                            )
+                            }
+                        </Box>
+                    </GridItem>
+
+                    <GridItem colSpan={2}>
+                     <Text fontWeight="bold">Skills:</Text>
+                     <Text>{(tutorData.user?.skills??[]).map(skill => skill.skillName).join(",")}</Text>
+                    </GridItem>
+                 </Grid>
+            </Box>
+            ) : (
+                <Text>No Qualifications and Skills found.</Text>  
+        );
+    default:
+        return null;
+    }
+};
+
 return(
     <>
     <Header />   
@@ -104,112 +306,29 @@ return(
     minH="100vh" 
     initial={{ opacity: 0 }} 
     animate={{ opacity: 1 }} 
-    transition={{ duration: 0.5 }}>
-
-    {/*Form Container */}
-    <Box>
-        <Text fontSize="3xl" fontWeight="bold" color="white" align="center">
+    transition={{ duration: 0.5 }}
+    display="flex"
+    justifyContent= "center"
+    >
+   
+   {/* Profile Box */}
+    <Box
+     width = "100%"
+     bg="whiteAlpha.900"
+     borderRadius="md"
+     boxShadow="md"
+     p={8}
+     >
+        <Text fontSize="3xl" fontWeight="bold" color="blue.700" mb={6} align="center">
             Your Profile
-       </Text>
-        <Box borderWidth="1px" borderRadius="md" p={6} background="gray.200">
-            <Text fontWeight="bold" mb={2}>
-                First Name: 
-                    <Text as="span" fontWeight="normal">{profileData.firstName}</Text>
-            </Text>
-
-            <Text fontWeight="bold" mb={2}>
-                Last Name: 
-                    <Text as="span" fontWeight="normal">{profileData.lastName}</Text>
-            </Text>
-
-            <Text fontWeight="bold" mb={2}>
-                Email: 
-                    <Text as="span" fontWeight="normal">{profileData.email}</Text>
-            </Text>
-
-            <Text fontWeight="bold" mb={2}>
-                Username: 
-                    <Text as="span" fontWeight="normal">{profileData.username}</Text>
-            </Text>
-            <Text fontWeight="bold" mb={2}>
-                Date of Joining: 
-                    <Text as="span" fontWeight="normal">{new Date(profileData.dateOfJoining).toLocaleDateString()}</Text>
-            </Text>
-
-            <Text mt={4} fontSize="sm" color="gray.400">
-               For security reasons, password is not displayed.
-            </Text>
-            
-        </Box>
-    </Box>
-
-    {tutorData && (
-    <Box>
-        <Text fontSize="3xl" fontWeight="bold" color="white" align="center">
-                Tutor Application Details
         </Text>
-       
-        <Box borderWidth="1px" borderRadius="md" p={6} background="gray.200">
-            <Text fontWeight="bold" mb={2}>
-                Roles: 
-                    <Text as="span" fontWeight="normal">{tutorData.sessionType}</Text>
-            </Text>
-
-            <Text fontWeight="bold" mb={2}>
-                Courses: 
-                     <Box pl = {4}>
-                    {Array.isArray(tutorData.courses) && tutorData.courses.length > 0 ? (
-                        tutorData.courses.map((courses,index) => (
-                        <Text key = {index} fontSize= "sm" mb={1}>
-                            - {courses.courseCode} {courses.courseName}
-                        </Text>
-                    ))
-                ): (
-                        <Text fontSize="sm">No Credentials Submitted</Text>
-                    )}
-                 </Box>
-            </Text>
-
-            <Text fontWeight="bold" mb={2}>
-                Previous Roles: 
-                    <Text as="span" fontWeight="normal">{tutorData.previousRoles.join(",")}</Text>
-            </Text>
  
-            <Text fontWeight="bold" mb={2}>
-                Availability:
-                    <Text as="span" fontWeight="normal">{tutorData.availability}</Text>
-            </Text>
+        {/* Passing tutor data as a prop */}
+        <ProfileHeader activeMenu={activeMenu} onMenuChange={setActiveMenu} tutorData={tutorData} />
 
-            <Text fontWeight="bold" mb={2}>
-                Skills:
-                    <Text as="span" fontWeight="normal">{(tutorData.user?.skills??[]).map(skill => skill.skillName).join(",")}</Text>
-            </Text>
-
-            <Text fontWeight="bold" mb={2}>
-                Academic Credentials:
-                 <Box pl = {4}>
-                    {Array.isArray(tutorData.user?.credentials) && tutorData.user?.credentials.length > 0 ? (
-                        tutorData.user?.credentials.map((cred,index) => (
-                        <Text key = {index} fontSize= "sm" mb={1}>
-                            - {cred.qualification} from {cred.institution} ({cred.year})
-                        </Text>
-                    ))
-                ): (
-                        <Text fontSize="sm">No Credentials Submitted</Text>
-                    )}
-                 </Box>
-            </Text>
-
-            <Text fontWeight="bold" mb={2}>
-                Submitted On:
-                   {new Date(tutorData.timestamp).toLocaleString()}
-            </Text>
-
-
-        </Box>
-
+        {renderActiveProfileSection()}
     </Box>
-    )}
+
     </MotionBox>
     <Footer />
     </>
