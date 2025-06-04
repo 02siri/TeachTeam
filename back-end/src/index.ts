7d88f39a2024337aecd0e4f2e92dfd7ebb602f8e
SrishtiKhosla <s3959795@student.rmit.edu.au>
2025-06-04 16:04:01 +1000

    index.ts updation

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -25,51 +25,15 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use(cors({
-//   origin: ['http://localhost:3000','http://localhost:3002'],
-//   credentials: true,
-//   // methods : ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-// }));
-
-// app.options('*', (req, res) => {
-//   res.sendStatus(200);
-// });
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    const allowedOrigins = ['http://localhost:3000', 'http://localhost:3002'];
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: ['http://localhost:3000','http://localhost:3002'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  exposedHeaders: ['Set-Cookie'],
+  // methods : ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
 }));
 
-// Handle preflight requests explicitly
-app.options('*', cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    
-    const allowedOrigins = ['http://localhost:3000', 'http://localhost:3002'];
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-}));
+app.options('*', (req, res) => {
+  res.sendStatus(200);
+});
 
 app.use(express.json());
 app.use("/api", userRoutes);
